c85d7a7fee293cdcf0c994e657c1cb350ec9258b
Meir Michanie <meirm@riunx.com>
2024-03-21 23:43:48 +0100

    Remove axios dependency.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import axios from 'axios';
 import Avatar from "../components/Avatar";
 import BotResponse from "../components/BotResponse";
 import Error from "../components/Error";
